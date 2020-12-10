9e4858c1af641021dae1872ccab0b2e4f1e16842
Nguyen Cong Sang <cs.ncsang@gmail.com>
2020-12-10 14:00:42 +0700

    first commit

---
+++ src/data_type.cpp
@@ -0,0 +1,14 @@
+#include <cmath>
+#include <iostream>
+#include <string>
+
+int main()
+{
+    // std::string file = "Hello";
+    // file.insert(3, "ddddd");
+    // file.erase(3, 5);
+    // std::cout << file << std::endl;
+    int number = 30;
+    std::cout << std::hex << number << std::endl;
+    return 0;
+}
